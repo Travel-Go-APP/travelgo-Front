09acbbc23ab70c7e8722e8918203525578ef1ba3
ww232330 <ww232330@gmail.com>
2024-01-08 22:58:38 +0900

    카카오지도기본 코드

---
+++ lib/View/map.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await dotenv.load(fileName: 'assets/env/.env');
+
+  /// 라이브러리 메모리에 appKey 등록
+  /// 지도가 호출되기 전에만 세팅해 주면 됩니다.
+  /// dotEnv 대신 appKey 를 직접 넣어주셔도 됩니다.
+  AuthRepository.initialize(appKey: dotenv.env['APP_KEY'] ?? '');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: MapPage(),
+    );
+  }
+}
+
+class MapPage extends StatefulWidget {
+  const MapPage({Key? key}) : super(key: key);
+
+  @override
+  State<MapPage> createState() => _MapState();
+}
+
+class _MapState extends State<MapPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body: KakaoMap(
+        onMapTap: (latLng) {
+          // ignore: avoid_print
+          print(latLng);
+        },
+      ),
+    );
+  }
+}
