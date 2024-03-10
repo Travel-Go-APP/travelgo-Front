41e80868972e779550061077ee049f3739d48080
junyoung park <j77777y@naver.com>
2024-03-11 01:41:20 +0900

    명소 UI 및 일부 코드 수정

--- lib/Attraction/ui/Attraction.dart
+++ lib/Attraction/ui/Attraction.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
-
 import 'package:animated_flip_counter/animated_flip_counter.dart';
 import 'package:countries_world_map/countries_world_map.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:travel_go/Attraction/function/attraction_info.dart';
+import 'package:travel_go/style.dart';
 import 'package:xtyle/xtyle.dart';
 
 class attractionPage extends StatefulWidget {
@@ -17,91 +18,205 @@ class attractionPage extends StatefulWidget {
 
 class _attractionPageState extends State<attractionPage> {
   final attractInfos = Get.put(attractInfo());
+  late ScrollController? _scrollController;
+  bool lastStatus = true;
+  double height = 200;
+  double percent = 0.83; // 현재 진행률
 
+  final List<String> country = <String>['서울', '부산', '대구', '인천', '광주', '대전', '울산', '세종', '경기', '강원', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도', '제주'];
+  
   @override
   void initState() {
+    super.initState();
     Timer(const Duration(milliseconds: 100), () async {
       setState(() {
-        attractInfos.updateValue();
+        attractInfos.updateValue(); // 명소 진행률 가져오기
       });
     });
-    super.initState();
+    _scrollController = ScrollController()
+      ..addListener(() {
+        if (_isShrink != lastStatus) {
+          setState(() {
+            lastStatus = _isShrink;
+          });
+        }
+      }); // 상단 스크롤 검사
+  }
+
+  bool get _isShrink {
+    return _scrollController != null && _scrollController!.hasClients && _scrollController!.offset > (height - kToolbarHeight);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
+      body: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (context, innerBoxIsScrolled) {
+          return [
+            appbar(), // 상단
+          ];
+        },
+        body: countryList(),
+      ),
+    );
+  }
+
+  // 상단
+  SliverAppBar appbar() {
+    return SliverAppBar(
+      floating: false, // 스크롤을 했을시 bottom만 보이게 하고 싶을때
+      pinned: true, // 스크롤을 했을시, 상단이 고정되게 하고 싶을때
+      leading: IconButton(
+        icon: const Icon(Icons.arrow_back_outlined),
+        color: Colors.black,
+        onPressed: () {
+          setState(() {
+            Navigator.of(context).pop();
+            attractInfos.percentValue = 0; // 진행률 초기화?
+          });
+        },
+      ),
+      title: const XtyleText("명소", style: TextStyle(fontWeight: FontWeight.bold)),
+      centerTitle: !_isShrink,
+      expandedHeight: MediaQuery.of(context).size.height * 0.7,
+      flexibleSpace: FlexibleSpaceBar(
         centerTitle: true,
-        title: const XtyleText("명소"),
-        leading: IconButton(
-            icon: const Icon(Icons.arrow_back_rounded),
-            onPressed: () {
-              setState(() {
-                Navigator.of(context).pop();
-                attractInfos.percentValue = 0;
-              });
-            }),
+        background: Container(
+            margin: EdgeInsets.only(top: MediaQuery.of(context).padding.top * 3.5),
+            child: Column(
+              children: [
+                koreanMaps(), // 대한민국 지도
+                percentText(), // 현재 진행률 (텍스트)
+                percentBar(), // 현재 진행률 (퍼센트)
+              ],
+            )),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.start,
-        children: [
-          Container(
-              margin: const EdgeInsets.only(top: 30),
-              height: MediaQuery.of(context).size.height * 0.5,
-              child: SimpleMap(
-                instructions: SMapSouthKorea.instructions,
-                countryBorder: const CountryBorder(color: Colors.grey), // 도시 경계선
-                defaultColor: Colors.white, // 기본 색상
-                colors: SMapSouthKoreaColors(
-                  kr11: Colors.red,
-                  kr26: Colors.blue,
-                  kr27: Colors.yellow,
-                  kr28: Colors.indigo,
-                  kr29: Colors.green,
-                  kr30: Colors.green,
-                  kr31: Colors.black,
-                  kr41: Colors.purple,
-                  kr42: Colors.brown,
-                  kr43: Colors.pink,
-                  kr44: Colors.deepPurple,
-                  kr45: Colors.red,
-                  kr46: Colors.orange,
-                  kr47: Colors.blue,
-                  kr48: Colors.indigo,
-                  kr49: Colors.orange,
-                  kr50: Colors.green,
-                ).toMap(),
-                callback: (id, name, tapDetails) {
-                  print("$name : $id"); // 이동 가능 할듯?
-                },
+      bottom: _isShrink
+          ? PreferredSize(
+              preferredSize: Size.fromHeight(MediaQuery.of(context).padding.top * 1),
+              child: Container(
+                  margin: EdgeInsets.all(MediaQuery.of(context).size.width * 0.01),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      const XtyleText("진행률", style: TextStyle(fontWeight: FontWeight.bold)),
+                      LinearPercentIndicator(
+                        width: MediaQuery.of(context).size.width * 0.7,
+                        percent: percent,
+                        progressColor: Colors.green,
+                      ),
+                      XtyleText(
+                        "${attractInfos.percentValue}%",
+                        style: const TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  )))
+          : null,
+    );
+  }
+
+  // 대한민국 지도
+  Widget koreanMaps() {
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.5,
+      margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.height * 0.03),
+      child: SimpleMap(
+        instructions: SMapSouthKorea.instructions,
+        countryBorder: const CountryBorder(color: Colors.grey), // 도시 경계선
+        defaultColor: Colors.white, // 기본 색상
+        colors: mapColor.toMap(),
+        callback: (id, name, tapDetails) {
+          print("$name : $id"); // 이동 가능 할듯?
+        },
+      ),
+    );
+  }
+
+  // 현재 진행률 (텍스트)
+  Widget percentText() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        XtyleText("전체 진행률", style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.06)),
+        AnimatedFlipCounter(
+          curve: Curves.decelerate,
+          wholeDigits: 2,
+          value: attractInfos.percentValue,
+          prefix: " : ",
+          suffix: "%",
+          duration: const Duration(milliseconds: 1400), // 퍼센트와 시간을 맞추기 위해 -100
+          textStyle: TextStyle(color: Colors.blue, fontFamily: 'GmarketSans', fontSize: MediaQuery.of(context).size.width * 0.06),
+        ),
+      ],
+    );
+  }
+
+  // 현재 진행률 (퍼센트)
+  Widget percentBar() {
+    return LinearPercentIndicator(
+      animation: true,
+      animationDuration: 1500,
+      padding: const EdgeInsets.only(left: 30, right: 30),
+      lineHeight: MediaQuery.of(context).size.height * 0.015,
+      percent: percent,
+      progressColor: Colors.green,
+      barRadius: Radius.circular(MediaQuery.of(context).size.width * 0.05),
+    );
+  }
+
+  // 지역 리스트
+  ListView countryList() {
+    return ListView.builder(
+      itemCount: country.length,
+      itemBuilder: (context, index) {
+        return Container(
+          height: MediaQuery.of(context).size.height * 0.08,
+          margin: EdgeInsets.all(MediaQuery.of(context).size.height * 0.005),
+          child: ElevatedButton(
+              style: ElevatedButton.styleFrom(elevation: 0, side: const BorderSide(color: Colors.white)),
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Stack(
+                        alignment: Alignment.center,
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.green,
+                            maxRadius: MediaQuery.of(context).size.height * 0.022,
+                          ),
+                          CircleAvatar(
+                            backgroundColor: Colors.white,
+                            maxRadius: MediaQuery.of(context).size.height * 0.02,
+                            child: const Icon(Icons.check, color: Colors.green),
+                          ),
+                        ],
+                      ),
+                      SizedBox(width: MediaQuery.of(context).size.width * 0.03),
+                      XtyleText(country[index], style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width * 0.055, color: Colors.black)),
+                    ],
+                  ),
+                  RichText(
+                    text: TextSpan(children: [
+                      WidgetSpan(
+                          child: XtyleText(
+                        "100%",
+                        style: TextStyle(fontWeight: FontWeight.bold, fontSize: MediaQuery.of(context).size.width * 0.045, fontFamily: 'GmarketSans', color: Colors.blue),
+                      )),
+                      WidgetSpan(
+                          child: XtyleText(
+                        "(10/10)",
+                        style: TextStyle(fontSize: MediaQuery.of(context).size.width * 0.03, fontFamily: 'GmarketSans', color: Colors.grey),
+                      )),
+                    ]),
+                  ),
+                ],
               )),
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const XtyleText("현재 진행률 : ", style: TextStyle(fontSize: 35, fontWeight: FontWeight.bold)),
-            AnimatedFlipCounter(
-              curve: Curves.decelerate,
-              wholeDigits: 2,
-              value: attractInfos.percentValue,
-              suffix: "%",
-              duration: const Duration(milliseconds: 1400), // 퍼센트와 시간을 맞추기 위해 -100
-            )
-          ]),
-          LinearPercentIndicator(
-            animation: true,
-            animationDuration: 1500,
-            lineHeight: MediaQuery.of(context).size.height * 0.015,
-            percent: 0.83,
-            progressColor: Colors.green,
-          ),
-          ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  attractInfos.updateValue2();
-                });
-              },
-              child: Text("변경"))
-        ],
-      ),
+        );
+      },
     );
   }
 }
--- lib/style.dart
+++ lib/style.dart
@@ -1,3 +1,4 @@
+import 'package:countries_world_map/data/maps/countries/south_korea.dart';
 import 'package:flutter/material.dart';
 
 /* main.dart */
@@ -30,3 +31,24 @@ ShapeBorder AppbarButtonShape = RoundedRectangleBorder(borderRadius: BorderRadiu
 Color AppbarBackColor = Colors.white;
 
 Color AppbarButtonColor = Colors.grey[200]!;
+
+/* Attraction.dart */
+SMapSouthKoreaColors mapColor = SMapSouthKoreaColors(
+    kr11: Colors.red, // 서울
+    kr26: Colors.blue, // 부산
+    kr27: Colors.yellow, // 대구
+    kr28: Colors.indigo, // 인천
+    kr29: Colors.green, // 광주
+    kr30: Colors.green, // 대전
+    kr31: Colors.black, // 울산
+    kr41: Colors.purple, // 경기도
+    kr42: Colors.brown, // 강원
+    kr43: Colors.pink, // 충청북도
+    kr44: Colors.deepPurple, // 충청남도
+    kr45: Colors.red, // 전라북도
+    kr46: Colors.orange, // 전라남도
+    kr47: Colors.blue, // 경상북도
+    kr48: Colors.indigo, // 경상남도
+    kr49: Colors.orange, // 제주
+    kr50: Colors.green, // 세종
+    );
